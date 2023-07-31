0236a999bd5913fde1e0a912859a7d338c1a4e3f
Alireza.k <ahmidyly@gmail.com>
2023-07-31 14:38:09 +0330

    Add description

---
+++ rock_paper_scissors.py
@@ -0,0 +1,14 @@
+import random
+
+my_choice = ["Stone", 'Paper', 'Scissors']
+
+print('Welcome to Stone, Paper and Scissors game.')
+print()
+
+### Add description
+print('Choose one of the options : ')
+print('1. One player')
+print('2. One player')
+
+user1 = input('Enter your choice : ')
+user2 = ran
